e097d5b9a665e44f5cf8113786e0822a0599201f
Tamika Nomara <taminomara@gmail.com>
2024-05-19 17:11:17 +0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,104 @@
+#!/usr/bin/env tsx
+
+import { serve } from "@hono/node-server";
+import { createBot } from "#root/bot/index.js";
+import { config } from "#root/config.js";
+import { logger } from "#root/logger.js";
+import { createServer } from "#root/server/index.js";
+import { AddressInfo } from "node:net";
+
+function onShutdown(cleanUp: () => Promise<void>) {
+  let isShuttingDown = false;
+  const handleShutdown = async () => {
+    if (isShuttingDown) return;
+    isShuttingDown = true;
+    logger.info("Shutdown");
+    await cleanUp();
+  };
+  process.on("SIGINT", handleShutdown);
+  process.on("SIGTERM", handleShutdown);
+}
+
+async function startPolling() {
+  const bot = createBot(config.BOT_TOKEN);
+
+  // graceful shutdown
+  onShutdown(async () => {
+    await bot.stop();
+  });
+
+  // start bot
+  await bot.start({
+    allowed_updates: config.BOT_ALLOWED_UPDATES,
+    onStart: ({ username }) =>
+      logger.info({
+        msg: "Bot running...",
+        username,
+      }),
+  });
+}
+
+async function startWebhook() {
+  const bot = createBot(config.BOT_TOKEN);
+  const server = await createServer(bot);
+
+  let serverHandle: undefined | ReturnType<typeof serve>;
+  const startServer = () =>
+    new Promise<AddressInfo>((resolve) => {
+      serverHandle = serve(
+        {
+          fetch: server.fetch,
+          hostname: config.BOT_SERVER_HOST,
+          port: config.BOT_SERVER_PORT,
+        },
+        (info) => resolve(info),
+      );
+    });
+  const stopServer = async () =>
+    new Promise<void>((resolve) => {
+      if (serverHandle) {
+        serverHandle.close(() => resolve());
+      } else {
+        resolve();
+      }
+    });
+
+  // graceful shutdown
+  onShutdown(async () => {
+    await stopServer();
+  });
+
+  // to prevent receiving updates before the bot is ready
+  await bot.init();
+
+  // start server
+  const info = await startServer();
+  logger.info({
+    msg: "Server started",
+    url:
+      info.family === "IPv6"
+        ? `http://[${info.address}]:${info.port}`
+        : `http://${info.address}:${info.port}`,
+  });
+
+  // set webhook
+  await bot.api.setWebhook(config.BOT_WEBHOOK, {
+    allowed_updates: config.BOT_ALLOWED_UPDATES,
+    secret_token: config.BOT_WEBHOOK_SECRET,
+  });
+  logger.info({
+    msg: "Webhook was set",
+    url: config.BOT_WEBHOOK,
+  });
+}
+
+try {
+  if (config.BOT_MODE === "webhook") {
+    await startWebhook();
+  } else if (config.BOT_MODE === "polling") {
+    await startPolling();
+  }
+} catch (error) {
+  logger.error(error);
+  process.exit(1);
+}
