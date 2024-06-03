e64d846112953adca0771d064c71206a34142931
Tamika Nomara <taminomara@gmail.com>
2024-06-03 15:25:16 +0400

    Cleanup

--- src/backend/migrations/Migration20240522085713.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20240522085713 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table `user` add column `interview_group_topic` integer null;');
-  }
-
-}
--- src/backend/migrations/Migration20240522141937.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20240522141937 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table `user` rename column `interview_group_topic` to `admin_group_topic`;');
-  }
-
-}
