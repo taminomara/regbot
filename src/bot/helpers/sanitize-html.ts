96fe09d376d6a16c2a267d0e03499e623b0ab0f1
Tamika Nomara <taminomara@gmail.com>
2024-10-10 02:32:56 +0400

    Control event description length

--- src/bot/helpers/sanitize-html.ts
+++ src/bot/helpers/sanitize-html.ts
@@ -5,11 +5,23 @@ export function sanitizeHtml(s: string | undefined | null): string | undefined;
 export function sanitizeHtml(s: string | undefined | null): string | undefined {
   return s === undefined || s === null
     ? undefined
-    : sanitize(s, { allowedTags: [], allowedAttributes: {} });
+    : sanitize(s, {
+        allowedTags: [],
+        allowedAttributes: {},
+        disallowedTagsMode: "discard",
+      });
 }
 
 export function sanitizeHtmlOrEmpty(s: string | undefined | null): string {
   return s === undefined || s === null
     ? "<i>&lt;empty&gt;</i>"
-    : sanitize(s, { allowedTags: [], allowedAttributes: {} });
+    : sanitize(s, {
+        allowedTags: [],
+        allowedAttributes: {},
+        disallowedTagsMode: "discard",
+      });
+}
+
+export function textLength(s: string | undefined | null): number {
+  return sanitizeHtmlOrEmpty(s).replaceAll(/&(lt|gt|amp);/g, "-").length;
 }
